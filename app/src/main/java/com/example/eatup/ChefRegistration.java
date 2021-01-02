88f6815b9cbe0a651b09e76b587f2e260aca8739
sadunidevindi <sanduni97dewindi@gmail.com>
2021-01-02 06:50:37 -0800

    Email Authentication & Saving Realtime Database part1

--- app/src/main/java/com/example/eatup/ChefRegistration.java
+++ app/src/main/java/com/example/eatup/ChefRegistration.java
@@ -1,14 +1,276 @@
 package com.example.eatup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hbb20.CountryCodePicker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChefRegistration extends AppCompatActivity {
+    String[] Gampaha={"gampaha","weliweriya","kadawatha","kirillawala"};
+    String[] Colombo={"kiribathgoda","colombo1","kelaniya","homagama"};
+
+    TextInputLayout Fname,Lname,Email,Pass,cpass,mobileno,houseno,area,pincode;
+    Spinner Statespin,cityspin;
+    Button signup,Emaill,phone;
+    CountryCodePicker Cpp;
+    FirebaseAuth FAuth;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    String fname,lname,emailid,password,confpassword,mobile,house,Area,Pincode,statee,cityy;
+    String role="Customer";
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_registration);
+
+        Fname=(TextInputLayout)findViewById(R.id.Firstname);
+        Lname=(TextInputLayout)findViewById(R.id.Lastname);
+        Email=(TextInputLayout)findViewById(R.id.Email);
+        Pass=(TextInputLayout)findViewById(R.id.password);
+        cpass=(TextInputLayout)findViewById(R.id.Cpass);
+        mobileno=(TextInputLayout)findViewById(R.id.mobileno);
+        houseno=(TextInputLayout)findViewById(R.id.houseNo);
+        area=(TextInputLayout)findViewById(R.id.Area);
+        pincode=(TextInputLayout)findViewById(R.id.Pincode);
+        Statespin=(Spinner) findViewById(R.id.Statee);
+        cityspin=(Spinner) findViewById(R.id.Citys);
+
+        signup = (Button)findViewById(R.id.Signup);
+        Emaill = (Button)findViewById(R.id.email);
+        phone = (Button)findViewById(R.id.phone);
+
+        Cpp = (CountryCodePicker)findViewById(R.id.CountryCode);
+
+        Statespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Object value = parent.getItemAtPosition(position);
+                statee = value.toString().trim();
+                if (statee.equals("Gampaha")) {
+                    ArrayList<String> list = new ArrayList<>();
+                    for (String cities : Gampaha) {
+                        list.add(cities);
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ChefRegistration.this, android.R.layout.simple_spinner_item, list);
+                    cityspin.setAdapter(arrayAdapter);
+                }
+                if (statee.equals("Colombo")) {
+                    ArrayList<String> list = new ArrayList<>();
+                    for (String cities : Colombo) {
+                        list.add(cities);
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ChefRegistration.this, android.R.layout.simple_spinner_item, list);
+                    cityspin.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        databaseReference = firebaseDatabase.getInstance().getReference("Customer");
+        FAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fname = Fname.getEditText().getText().toString().trim();
+                lname = Lname.getEditText().getText().toString().trim();
+                emailid = Email.getEditText().getText().toString().trim();
+                mobile = mobileno.getEditText().getText().toString().trim();
+                password = Pass.getEditText().getText().toString().trim();
+                confpassword = cpass.getEditText().getText().toString().trim();
+                Area = area.getEditText().getText().toString().trim();
+                house= houseno.getEditText().getText().toString().trim();
+                Pincode = pincode.getEditText().getText().toString().trim();
+            }
+        });
+
+         if (isvalid){
+             final ProgressDialog mDialog = new ProgressDialog(ChefRegistration.this);
+             mDialog. setCancelable(false);
+             mDialog.setCanceledOnTouchOutside(false);
+             mDialog.setMessage("Registation is progress please wait.....");
+             mDialog.show();
+
+             FAuth.createUserWithEmailAndPassword(emailid,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                 @Override
+                 public void onComplete(@NonNull Task<AuthResult> task) {
+                     if (task.isSuccessful() ){
+                         String useridd= FirebaseAuth.getInstance().getCurrentUser().getUid();
+                         databaseReference=FirebaseDatabase.getInstance().getReference("user" ).child(useridd);
+                         final HashMap<String,String> hashMap=new HashMap<>();
+                         hashMap.put("Role",role);
+                         databaseReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                             @Override
+                             public void onComplete(@NonNull Task<Void> task) {
+                                 HashMap<String,String> hashMap1=new HashMap<>();
+                                 hashMap1.put("First Name",fname);
+                                 hashMap1.put("Last Name",lname);
+                                 hashMap1.put("EmailId",emailid);
+                                 hashMap1.put("City",cityy);
+                                 hashMap1.put("Area",Area);
+                                 hashMap1.put("Password",password);
+                                 hashMap1.put("PinCode",Pincode);
+                                 hashMap1.put("State",statee);
+                                 hashMap1.put("Confirm Password",confpassword);
+                                 hashMap1.put("House",house);
+
+
+
+                             }
+                         });
+
+                     }
+
+                 }
+             });
+
+
+         }
+            
+        }
+
+        String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        public boolean isValid(){
+            Email.setErrorEnabled(false);
+            Email.setError("");
+            Fname.setErrorEnabled(false);
+            Fname.setError("");
+            Lname.setErrorEnabled(false);
+            Lname.setError("");
+            Pass.setErrorEnabled(false);
+            Pass.setError("");
+            mobileno.setErrorEnabled(false);
+            mobileno.setError("");
+            cpass.setErrorEnabled(false);
+            cpass.setError("");
+            area.setErrorEnabled(false);
+            area.setError("");
+            houseno.setErrorEnabled(false);
+            houseno.setError("");
+            pincode.setErrorEnabled(false);
+            pincode.setError("");
+
+            boolean isvalid=false,isValidname=false,isValidlname=false,isValidemail=false,isValidpassword=false,isValidconfpassword=false,isValidarea=false,isValidlocaladd=false,isValidpincode=false,isValidmobilenum=false;
+            if(TextUtils.isEmpty(fname)){
+                Fname.setErrorEnabled(true);
+                Fname.setError("Enter First Name");
+            }else{
+                isValidname = true;
+            }
+            if(TextUtils.isEmpty(lname)){
+                Lname.setErrorEnabled(true);
+                Lname.setError("Enter Last Name");
+            }else{
+                isValidlname = true;
+            }
+            if(TextUtils.isEmpty(emailid)){
+                Email.setErrorEnabled(true);
+                Email.setError("Email Is Required");
+            }else{
+                if(emailid.matches(emailpattern)){
+                    isValidemail = true;
+                }else{
+                    Email.setErrorEnabled(true);
+                    Email.setError("Enter a Valid Email Id");
+                }
+            }
+            if(TextUtils.isEmpty(password)){
+                Pass.setErrorEnabled(true);
+                Pass.setError("Enter Password");
+            }else{
+                if(password.length()<8){
+                    Pass.setErrorEnabled(true);
+                    Pass.setError("Password is Weak");
+                }else{
+                    isValidpassword = true;
+                }
+            }
+            if(TextUtils.isEmpty(confpassword)){
+                cpass.setErrorEnabled(true);
+                cpass.setError("Enter Password Again");
+            }else{
+                if(!password.equals(confpassword)){
+                    cpass.setErrorEnabled(true);
+                    cpass.setError("Password Dosen't Match");
+                }else{
+                    isValidconfpassword = true;
+                }
+            }
+            if(TextUtils.isEmpty(mobile)){
+                mobileno.setErrorEnabled(true);
+                mobileno.setError("Mobile Number Is Required");
+            }else{
+                if(mobile.length()<10){
+                    mobileno.setErrorEnabled(true);
+                    mobileno.setError("Invalid Mobile Number");
+                }else{
+                    isValidmobilenum = true;
+                }
+            }
+            if(TextUtils.isEmpty(Area)){
+                area.setErrorEnabled(true);
+                area.setError("Area Is Required");
+            }else{
+                isValidarea = true;
+            }
+            if(TextUtils.isEmpty(Pincode)){
+                pincode.setErrorEnabled(true);
+                pincode.setError("Please Enter Pincode");
+            }else{
+                isValidpincode = true;
+            }
+            if(TextUtils.isEmpty(house)){
+                houseno.setErrorEnabled(true);
+                houseno.setError("Fields Can't Be Empty");
+            }else{
+                isValidlocaladd = true;
+            }
+
+            isValid = (isValidarea && isValidconfpassword && isValidpassword && isValidpincode && isValidemail && isValidmobilenum && isValidname && isValidlocaladd && isValidlname) ? true : false;
+            return isvalid;
+
+        cityspin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object value=arent.getItemAtPosition(position);
+                cityy = value.toString().trim();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        }
+
+
     }
 }
